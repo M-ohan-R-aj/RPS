c772173185b5921e512cd567a0577a25465ed48b
M-ohan-R-aj <manickmohan7@gmail.com>
2022-09-11 19:19:09 +0530

    Day-1

--- script.js
+++ script.js
@@ -1,55 +1,91 @@
 let playerScore=0;
 let computerScore=0;
-
+const rockButton=document.querySelector(".rock");
+const paperButton=document.querySelector(".paper");
+const scissorsButton=document.querySelector(".scissors");
+const result=document.querySelector(".result")
 const computerPlay = () => {
     const arrOfChoices = ['rock', 'paper', 'scissors']
     const randomNum = Math.floor(Math.random()*3)
     return arrOfChoices[randomNum]
 }
 const playRound = (playerSelection, computerSelection)=>{
+    console.log("1", playerSelection , "2", computerSelection);
     if (computerSelection===playerSelection){
-        return `It's a tie! You both chose ${playerSelection}`
+        const output=document.createElement("p")
+        outcome.innertext=`It's a tie! You both chose ${playerSelection}`
+        result.appendChild(p)
     }
     else if (computerSelection==="rock"&& playerSelection==="paper"){
         playerScore++;
-        return "You won! Paper beats rock!!!"
+        const output=document.createElement("p")
+        outcome.innertext="You won! Paper beats rock!!!"
+        result.appendChild(p)
     }
     else if (computerSelection==="rock"&& playerSelection==="scissors"){
         computerScore++;
-        return "Computer won! Rock beats scissors!!!"
+        const output=document.createElement("p")
+        t.createElement("p")
+        outcome.innertext="Computer won! Rock beats scissors!!!"
+        result.appendChild(p)
     }
     else if (computerSelection==="paper"&& playerSelection==="scissors"){
         playerScore++;
-        return "You won! Scissors beat paper!!!"
+        const output=document.createElement("p")
+        outcome.innertext="You won! Scissors beat paper!!!"
+        result.appendChild(p)
     }
     else if (playerSelection==="rock"&& computerSelection==="paper"){
         computerScore++;
-        return "Computer won! Paper beats rock!!!"
+        const output=document.createElement("p")
+        outcome.innertext="Computer won! Paper beats rock!!!"
+        result.appendChild(p)
     }
     else if (playerSelection==="rock"&& computerSelection==="scissors"){
         playerScore++;
-        return "You won! Rock beat Scissors!!!"
+        const output=document.createElement("p")
+        outcome.innertext="You won! Rock beat Scissors!!!"
+        result.appendChild(p)
     }
     else if (playerSelection==="paper"&& computerSelection==="scissors"){
         computerScore++;
-        return "Computer won! Scissors beats paper!!!"
-    }
-}
-const game = () =>{
-    for (let i=0; i<5; i++){
-    const  playerSelection = prompt("Choose one. Rock, paper or scissors").toLowerCase();
-    const computerSelection=computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-        }
-        if (playerScore>computerScore){
-            return "You beat the computer! 5Head"
-        }
-        else if (playerScore<computerScore){
-            return "You lost to the computer"
-    }
-    else {
-        return "It's  a draw"
+        const output=document.createElement("p")
+        outcome.innertext="Computer won! Scissors beats paper!!!"
+        result.appendChild(p)
     }
 }
 
-console.log(game());
+rockButton.addEventListener('click', ()=>{
+    const computerSelection=computerPlay();
+    const playerSelection="rock";
+    playRound(playerSelection, computerSelection)
+})
+paperButton.addEventListener('click', ()=>{
+    const computerSelection=computerPlay();
+    const playerSelection="paper";
+    playRound(playerSelection, computerSelection)
+})
+scissorsButton.addEventListener('click', ()=>{
+    const computerSelection=computerPlay();
+    const playerSelection="scissors";
+    playRound(playerSelection, computerSelection)
+})
+// const game = () =>{
+//     for (let i=0; i<5; i++){
+//     var  playerSelection = document.getElementsByClassName("input")
+//     console.log(playerSelection)
+//     const computerSelection=computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+//         }
+//         if (playerScore>computerScore){
+//             return "You beat the computer! 5Head"
+//         }
+//         else if (playerScore<computerScore){
+//             return "You lost to the computer"
+//     }
+//     else {
+//         return "It's  a draw"
+//     }
+// }
+
+// console.log(game());
